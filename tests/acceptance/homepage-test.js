b222670fca5a874f3083c36352137de3a4c31a8b
SiyangHu <shu1@tulane.edu>
2019-02-08 10:34:55 -0600

    Defined the test cases for homepage

---
+++ tests/acceptance/homepage-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | homepage', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('Should link to symptoms page.', async function (assert){});
+
+  test('Should link to public health information page.'), async function (assert){});
+
+  test('Should link to about page.', async function (assert){});
+
+  test('Should link to contact information.', async function (assert){});
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+  });
+});
